dee6b23f97700a9e34e3f9bd72dd2153784e077d
Tomas Rivera <tomasrivera.1994@gmail.com>
2022-06-09 20:53:57 +0200

    Sexta Version

--- proyecto_coder/app_coder/admin.py
+++ proyecto_coder/app_coder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Profesores)
+admin.site.register(Alumnos)
+admin.site.register(Entregable)
